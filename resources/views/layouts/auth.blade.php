9b906d86c43056a4fddbba8c4366aa190f540cff
Ahmad Fauzi <uzif8100@students.amikom.ac.id>
2024-01-18 22:29:37 +0700

    Auth Fortify

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shorcut icon" href="{{ asset('assets/img/company.png') }}" type="image/x-icon">
+    <title>{{ $title ?? config('app.name') }} - Admin</title>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="{{ asset('assets/css/sb-admin-2.min.css') }}">
+</head>
+<body style="background-color: #e2e8f0;">
+    @yield('content')
+
+    <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
+</body>
+</html>
